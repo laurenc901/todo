3f4f5ae44e123aec50112f450ebc5747846bd31c
Lauren Cox <98595158+laurenc901@users.noreply.github.com>
2022-02-22 19:51:07 -0600

    updated app.js

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-const form = document.querySelector("form");
-const todos = document.querySelector('#todos');
-const todoAdd = document.querySelector("#todoAdd");
-let doneBtn = document.createElement("button");
-let deleteBtn = document.createElement("button");
-doneBtn.innerText = "Done!";
-deleteBtn.innerText = "Delete";
-
-
-
-
-const savedTodos = JSON.parse(localStorage.getItem("list")) || [];
-for (let i = 0; i < savedTodos.length; i++) {
-  let newLI = document.createElement("li");
-  newLI.innerText = savedTodos[i].task;
-  newLI.isDone = savedTodos[i].isDone ? true : false;
-  if (newLI.isDone) {
-    newLI.style.textDecoration = "line-through";
-  }
+  const form = document.querySelector("form");
+  const todos = document.querySelector('.todos');
+  const todoAdd = document.querySelector("#todoAdd");
+  const list = [];
   
-  todos.appendChild(newLI);
-  newLI.append(doneBtn);
-  newLI.append(deleteBtn);
-}
+  form.addEventListener("submit", function(e){
+      e.preventDefault();
+      makeList();
+      addToStorage();
+      form.reset();
+      });
+
+      function makeList(){
+      const newLI = document.createElement("li");
+      const deleteBtn = document.createElement("button");
+      newLI.innerText = todoAdd.value; 
+      deleteBtn.innerText = "Delete";
+      newLI.append(deleteBtn);
+      todos.append(newLI);
+      }
 
 
-
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-    let newLI = document.createElement("LI");
-    newLI.innerText = todoAdd.value;
-    savedTodos.push({ task: newLI.innerText, isDone: false });
-  localStorage.setItem("list", JSON.stringify(savedTodos));
-    
-    newLI.isDone = false;
-    form.reset();
-    todos.appendChild(newLI);
-   
-    newLI.append(doneBtn);
-    newLI.append(deleteBtn);
-    });
-
-
-   
+  
       todos.addEventListener('click', function(e){
-        if (e.target === doneBtn){
-      e.target.parentElement.style.textDecoration = 'line-through';
-      e.target.isDone = true;
-        }
-        if (e.target === deleteBtn){
+        if (e.target.tagName === "LI"){
+      e.target.classList.add('done');
+        } else if (e.target.tagName === 'BUTTON'){
         e.target.parentElement.remove();
         }
-  });
+    })
+
+    
+    function addToStorage(){
+     
+      list.push({task: todoAdd.value});
+      localStorage.setItem("items", JSON.stringify(list));
+   }
+
+   const savedTodos = JSON.parse(localStorage.getItem('items')) ;
   
-
-})
-
+   
+   for (let i = 0; i < savedTodos.length; i++) {
+    const newLI = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    newLI.innerText = savedTodos[i].task;
+    deleteBtn.innerText = "Delete";
+    newLI.append(deleteBtn);
+    todos.append(newLI);
+   }
+    
+  }) 
+  
